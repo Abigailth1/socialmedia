2a2b44b552e191d6a09f449fa82c8be9a1e06317
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-07-14 15:40:21 -0400

    Added more Firebase Modules

--- socialmedia/socialmediaApp.swift
+++ socialmedia/socialmediaApp.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
