cf8f17010ed8880efe724529fbbf6521cae2d9e4
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-07-18 15:15:41 -0400

    Added new groupsto implement MVVM

--- socialmedia/Model/User.swift
+++ socialmedia/Model/User.swift
@@ -18,10 +18,10 @@ struct User: Identifiable, Codable {
 
 extension User {
     static var MOCK_USERS: [User] = [
-        .init(id: NSUUID().uuidString, username: "batman", profileImageUrl: <#T##String?#>, fullname: "Bruce Wayne", bio: "Gothman's Dark Knight", email: "batman@gmail.com"),
-        .init(id: NSUUID().uuidString, username: "venom", profileImageUrl: <#T##String?#>, fullname: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
-        .init(id: NSUUID().uuidString, username: "ironman", profileImageUrl: <#T##String?#>, fullname: "Tony Stark", bio: "Playboy & Billionaire", email: "ironman@gmail.com"),
-        .init(id: NSUUID().uuidString, username: "blackpanther", profileImageUrl: <#T##String?#>, fullname: "Bruce Wayne", bio: "Test Bio", email: "blackpanther@gmail.com"),
-        .init(id: NSUUID().uuidString, username: "spiderman", profileImageUrl: <#T##String?#>, fullname: "Peter Parker", bio: "Test Bio", email: "spiderman@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "batman", profileImageUrl: "na", fullname: "Bruce Wayne", bio: "Gothman's Dark Knight", email: "batman@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "venom", profileImageUrl: "na", fullname: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "ironman", profileImageUrl: "na", fullname: "Tony Stark", bio: "Playboy & Billionaire", email: "ironman@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "blackpanther", profileImageUrl: "na", fullname: "Bruce Wayne", bio: "Test Bio", email: "blackpanther@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "spiderman", profileImageUrl: "na", fullname: "Peter Parker", bio: "Test Bio", email: "spiderman@gmail.com"),
     ]
 }
